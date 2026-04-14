dca9831cd55a9e22c821748cf1966370d697e2eb
Marlon <107611839+MarlonAACN@users.noreply.github.com>
2026-04-14 13:38:18 +0200

    Hotfix: V.1.0.1 (#4)

--- packages/icons/src/outline/NumberIcon.tsx
+++
@@ -1,9 +0,0 @@
-import type { SVGProps } from 'react';
-
-const NumberIcon = (props: SVGProps<SVGSVGElement>) => (
-  <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
-    <path d="M7.79281 4.66851C8.42249 4.03792 9.50055 4.48353 9.50082 5.37456V13.2339C9.50042 13.5097 9.27663 13.7338 9.00082 13.7339C8.72492 13.7339 8.50122 13.5097 8.50082 13.2339V5.37456L6.85336 7.02398C6.6583 7.21911 6.34169 7.21976 6.14632 7.02495C5.95104 6.82989 5.95141 6.51332 6.14632 6.31792L7.79281 4.66851Z" fill="currentColor"/>
-  </svg>
-);
-
-export default NumberIcon;
