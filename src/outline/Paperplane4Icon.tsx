2f6663ed1e4d0e0e2eca34e78f3213baa023b398
Marlon Kerth <MarlonAACN@users.noreply.github.com>
2026-05-13 12:14:25 +0200

    Release V.1.2.1

--- src/outline/Paperplane4Icon.tsx
+++ src/outline/Paperplane4Icon.tsx
@@ -1,6 +1,6 @@
 import type { SVGProps } from 'react';
 
-export const PaperplaneIcon3Icon = (props: SVGProps<SVGSVGElement>) => (
+export const Paperplane4Icon = (props: SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width="18"
