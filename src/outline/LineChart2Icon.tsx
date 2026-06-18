aa604e28d4789fe2b7ab7c8ab7aed45bf972aba2
Marlon Kerth <MarlonAACN@users.noreply.github.com>
2026-06-18 17:04:22 +0200

    Add missing icons 2

--- src/outline/LineChart2Icon.tsx
+++ src/outline/LineChart2Icon.tsx
@@ -1,6 +1,6 @@
 import type { SVGProps } from 'react';
 
-function LineChartIcon(props: SVGProps<SVGSVGElement>) {
+function LineChart2Icon(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
       width="24"
@@ -13,20 +13,21 @@ function LineChartIcon(props: SVGProps<SVGSVGElement>) {
       <path
         d="M4.06763 19.9316L19.9318 19.9316"
         stroke="currentColor"
-        stroke-linecap="round"
+        strokeLinecap="round"
       />
       <path
         d="M4.06763 4.06738L4.06762 19.9316"
         stroke="currentColor"
-        stroke-linecap="round"
+        strokeLinecap="round"
       />
       <path
         d="M4.23438 15.4445L7.56232 12.1156C8.08298 11.5948 8.92729 11.5947 9.44803 12.1154L11.3331 14.0006C11.8538 14.5212 12.698 14.5212 13.2187 14.0006L18.8604 8.35889"
         stroke="currentColor"
-        stroke-linecap="round"
+        strokeLinecap="round"
       />
+      <circle cx="19.9371" cy="7.30414" r="2.38275" fill="currentColor" />
     </svg>
   );
 }
 
-export { LineChartIcon };
+export { LineChart2Icon };
