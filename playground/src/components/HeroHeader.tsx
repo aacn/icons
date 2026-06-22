9d67dbc758d7fd765ee1e258b0b8baadd520cc3c
Christos Rougalas <149603380+Drizzy13@users.noreply.github.com>
2026-06-22 13:47:32 +0200

    Chore: Added two missed icons (#19)

--- playground/src/components/HeroHeader.tsx
+++ playground/src/components/HeroHeader.tsx
@@ -37,7 +37,7 @@ export function HeroHeader({
                 onVersionChange(event.target.value as VersionOption)
               }
             >
-              <option value="latest">latest (v3.1.0)</option>
+              <option value="latest">latest (v3.1.1)</option>
               <option value="2.4.0">v2.4.0</option>
               <option value="2.2.0">v2.2.0</option>
               <option value="2.0.0">v2.0.0</option>
